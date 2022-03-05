183a36730fc9d3b52a688961d1e1deff33f219fc
Stephan Kromberg <s.kromberg@clevershuttle.de>
2022-03-05 23:13:20 +0100

    feat: Append lint-commits.yml to project

---
+++ .github/configs/commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-case": [1, "always", "upper-case"],
+    "subject-case": [1, "always", "sentence-case"],
+    "body-max-line-length": [1, "always", 100],
+  },
+};
