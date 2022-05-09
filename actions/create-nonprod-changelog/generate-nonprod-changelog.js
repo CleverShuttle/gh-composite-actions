32f8930dc99564c8ff37f6f6235028ade3402f86
Nils Rommelfanger <n.rommelfanger@clevershuttle.de>
2022-05-09 08:14:55 +0200

    Revert "refactor: use merge instead of rebase and no squashing"
    This reverts commit f67e40d43917cabe8d505205ed4b841085e3d3e9.

---
+++ actions/create-nonprod-changelog/generate-nonprod-changelog.js
@@ -0,0 +1,25 @@
+/**
+ * Script to generate the changelog for nonprod deployments
+ *
+ * usage:
+ *   node generate-nonprod-changelog.js REPO_BASE_URL
+ */
+const { execSync } = require("child_process");
+
+const repoBaseUrl = process.argv[2];
+
+if (!repoBaseUrl) {
+  console.error("REPO_BASE_URL argument was not provided!");
+  process.exit(1);
+}
+
+const branches = execSync("git log origin/master..HEAD --oneline | cut -d \" \" -f 2-").toString('utf8');
+
+const isNotEmpty = s => s !== "";
+
+const output = branches
+  .split("\n")
+  .filter(isNotEmpty)
+  .reduce((result, branchName) => `${result}\n* [${branchName}](${repoBaseUrl}/tree/${branchName})`, "")
+
+console.log(output);
